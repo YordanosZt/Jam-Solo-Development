5df39a8e1d6b344990d4b2266bfe7d53a8066958
Yordanos <yordanoszt15@gmail.com>
2024-12-16 09:32:14 +0300

    spiky platforms

---
+++ Entities/Traps/ice/spiky_platforms.gd
@@ -0,0 +1,35 @@
+extends StaticBody2D
+
+var can_spin: bool = false
+var rot_speed: float = 300.0
+var dxn: int = 1
+@onready var return_timer = $ReturnTimer
+@onready var start_spin_timer = $StartSpinTimer
+
+func _physics_process(delta):
+	if can_spin:
+		spin(delta)
+
+func spin(delta) -> void:
+	rotation_degrees += dxn * rot_speed * delta
+	
+	if rotation_degrees >= 180.0:
+		rotation_degrees = 180.0
+		can_spin = false
+		return_timer.start()
+		dxn = -1
+	if rotation_degrees <= 0:
+		rotation_degrees = 0
+		can_spin = false
+		dxn = 1
+
+func _on_check_player_body_entered(body):
+	start_spin_timer.start()
+
+
+func _on_return_timer_timeout():
+	can_spin = true
+
+
+func _on_start_spin_timer_timeout():
+	can_spin = true
